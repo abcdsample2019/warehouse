af71e29e5bdff5110ae69c4afcdb967148af14f1
Raghu <Raghu@Raghu-PC>
2019-07-25 11:56:50 +0530

    WH-856 : Employee Update

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -2,5 +2,20 @@ package com.app;
 
 public class Employee {
 
-	private int empId;
+	private double empSal;
+	private String empName;
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
 }
